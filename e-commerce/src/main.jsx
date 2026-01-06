e6c6589dc113dda4b22a2387e335f1e339892d01
rishikarjn <rishikasingh2109@gmail.com>
2026-01-06 23:17:41 +0530

    Integrate react-router dom

--- e-commerce/src/main.jsx
+++ e-commerce/src/main.jsx
@@ -1,35 +1,34 @@
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Products from './components/Products.jsx'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Products from './components/Products.jsx'
+import SingleProduct from './components/SingleProduct.jsx'
 
-
-const router =createBrowserRouter([
+const router = createBrowserRouter([
   {
     path:'/',
-    element:<App/>,    
+    element: <App/>,
     children:[
       {
-         path: '/',
-         element: <Home/>
-  },
-  {
-    path:'/products',
-    element:<Products/>
-  }
+        path:'/',
+        element:<Home/>
+      },
+      {
+        path:'/products',
+        element:<Products/>
+      },
+      {
+        path:'/products/:id',
+        element:<SingleProduct/>
+      }
     ]
   }
 ])
 
 createRoot(document.getElementById('root')).render(
-
   <>
-
-  <RouterProvider router={router}/>   
-  
-     </>
-
-  
-)
+    <RouterProvider router={router}/>
+  </>,
+) 
